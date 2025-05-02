912f447430214edb1d984a269f924d0a5dddd23d
TebohoMajake <112713050+TebohoMajake@users.noreply.github.com>
2025-05-02 22:37:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const gameNights = [
      {
          date: "2025-04-26",
          results: ["Mogale", "Gomo", "Teboho", "Ota", "Africa", "Beckham"]
+     },
+     {
+          date: "2025-05-02",
+          results: ["", "", "", "Africa", "Ota", "Teboho"]
      }
  ];
  
